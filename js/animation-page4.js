2d1d9f2d27a73c6d26f4a9451546e8c39109d21d
DaupinDavid <david_945@live.fr>
2026-03-08 01:59:47 +0100

    Ajout page 4

---
+++ js/animation-page4.js
@@ -0,0 +1,45 @@
+const images = [
+  "./images/Boite2.png",
+  "./images/Boite3.png",
+  "./images/Boite4.png"
+];
+
+images.forEach((src) => {
+  const img = new Image();
+  img.src = src;
+});
+
+const imageCadeau = document.querySelector(".img-boite");
+
+imageCadeau.classList.add('shake-active');
+
+function appliquerChangement(nouvelleSource) {
+  
+  imageCadeau.classList.add('pop-effect');
+  
+  setTimeout(() => {
+    imageCadeau.src = nouvelleSource;
+    imageCadeau.classList.remove('pop-effect');
+  }, 300);
+}
+
+setTimeout(() => {
+  
+  imageCadeau.classList.remove('shake-active');
+  appliquerChangement("./images/Boite2.png");
+
+  setTimeout(() => {
+    appliquerChangement("./images/Boite3.png");
+
+    setTimeout(() => {
+      appliquerChangement("./images/Boite4.png");
+
+    }, 2000);
+
+  }, 2000);
+
+}, 2000);
+
+setTimeout(() => {
+  window.location.href = "page5-resultat.html";
+}, 8000);
